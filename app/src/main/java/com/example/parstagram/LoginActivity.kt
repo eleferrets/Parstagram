038681d363f9dc8753f9bfc703d2366993123b11
Brian Balthazar <40572853+eleferrets@users.noreply.github.com>
2022-10-13 11:08:59 -0400

    Initial commit

---
+++ app/src/main/java/com/example/parstagram/LoginActivity.kt
@@ -0,0 +1,74 @@
+package com.example.parstagram
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.parse.ParseUser
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        
+        // Check if logged in
+        // If true, go to main activity
+        if (ParseUser.getCurrentUser() != null) {
+            goToMainActivity()
+        }
+
+        findViewById<Button>(R.id.login_button).setOnClickListener{
+            val username = findViewById<EditText>(R.id.et_username).text.toString()
+            val password = findViewById<EditText>(R.id.et_password).text.toString()
+            loginUser(username, password)
+        }
+        findViewById<Button>(R.id.signupBtn).setOnClickListener{
+            val username = findViewById<EditText>(R.id.et_username).text.toString()
+            val password = findViewById<EditText>(R.id.et_password).text.toString()
+            signUpUser(username, password)
+        }
+    }
+
+    private fun loginUser(username: String, password: String) {
+        ParseUser.logInInBackground(username, password, ({ user, e ->
+            if (user != null) {
+                Log.i(TAG, "Successfully logged in user")
+                goToMainActivity()
+            } else {
+                e.printStackTrace()
+                Toast.makeText(this, "Error logging in", Toast.LENGTH_SHORT).show()
+            }})
+        )
+    }
+    private fun signUpUser(username: String, password: String) {
+        // Create the ParseUser
+        val user = ParseUser()
+
+// Set fields for the user to be created
+        user.setUsername(username)
+        user.setPassword(password)
+
+        user.signUpInBackground { e ->
+            if (e == null) {
+                // Signed in
+                // Navigate to mainactivity
+                // Show toast to say successfully signed up for account
+            } else {
+                e.printStackTrace()
+                // Show toast for sign up was not successful
+            }
+        }
+    }
+    private fun goToMainActivity() {
+        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+        startActivity(intent)
+        // End app after using back button by closing this activity
+        finish()
+    }
+    companion object {
+        const val TAG = "LoginActivity"
+    }
+}
---
+++ app/src/main/java/com/example/parstagram/MainActivity.kt
@@ -0,0 +1,189 @@
+package com.example.parstagram
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import com.parse.*
+import java.io.File
+
+
+/**
+ * Let user create a post by taking a camera photo
+ */
+class MainActivity : AppCompatActivity() {
+    val CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034
+    val photoFileName = "photo.jpg"
+    var photoFile: File? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Set description of post
+        // Button to launch camera for picture
+        // Image to show the picture
+        // A button to save and send the post
+        findViewById<Button>(R.id.btnSubmit).setOnClickListener {
+            // Send post to server
+            // Get description
+            val description = findViewById<EditText>(R.id.description).text.toString()
+            val user = ParseUser.getCurrentUser()
+            if (photoFile != null) {
+                // Double exclamation points mean file is guaranteed not to be null
+                submitPost(description, user, photoFile!!)
+            }else
+            {
+                // Print error log message
+                // Show toast to tell user to take a picture
+            }
+        }
+        findViewById<Button>(R.id.btnTakePicture).setOnClickListener {
+            // Launch camera to let user take picture
+            onLaunchCamera()
+        }
+        queryPosts()
+    }
+
+    // Inflate the menu to show the app bar icon
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+// What happens when this is clicked
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.logout) {
+            logoutUser()
+            Toast.makeText(this, "Logged out", Toast.LENGTH_SHORT).show()
+goToLoginActivity()
+        }
+    return super.onOptionsItemSelected(item)
+    }
+    private fun goToLoginActivity() {
+        val intent = Intent(this@MainActivity, LoginActivity::class.java)
+        startActivity(intent)
+        // End app after using back button by closing this activity
+        finish()
+    }
+    private fun logoutUser() {
+        ParseUser.logOut()
+        val currentUser = ParseUser.getCurrentUser() // this will now be null
+    }
+    // Send a post object to our Parse server
+    private fun submitPost(description: String, user: ParseUser, file: File) {
+        // Create the Post object
+        val post = Post()
+        post.setDescription(description)
+        post.setUser(user)
+        post.setImage(ParseFile(file))
+        post.saveInBackground{exception ->
+            if (exception != null) {
+                Log.e(TAG, "Error while saving post")
+                exception.printStackTrace()
+                // Show a toast to tell user something is wrong
+            }
+            else {
+                Log.i(TAG, "Successfully saved post")
+                // Reset EditText to be empty
+                // Reset ImageView to be empty
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // We have photo on disk
+                val takenImage = BitmapFactory.decodeFile(photoFile!!.absolutePath)
+                // Resize bitmap
+                // Load the taken image into a preview
+                val ivPreview: ImageView = findViewById(R.id.imageView)
+                ivPreview.setImageBitmap(takenImage)
+            } else {
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun onLaunchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName)
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        if (photoFile != null) {
+            val fileProvider: Uri =
+                FileProvider.getUriForFile(this, "com.codepath.fileprovider", photoFile!!)
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
+
+            // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+            // So as long as the result is not null, it's safe to use the intent.
+
+            // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+            // So as long as the result is not null, it's safe to use the intent.
+            if (intent.resolveActivity(packageManager) != null) {
+                // Start the image capture intent to take photo
+                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE)
+            }
+        }
+    }
+    // Returns the File for a photo stored on disk given the fileName
+    fun getPhotoFileUri(fileName: String): File {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        val mediaStorageDir =
+            File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG)
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
+            Log.d(TAG, "failed to create directory")
+        }
+
+        // Return the file target for the photo based on filename
+        return File(mediaStorageDir.path + File.separator + fileName)
+    }
+
+    // Query for all posts
+    fun queryPosts(){
+        // Specify which class to query
+        val query: ParseQuery<Post> = ParseQuery.getQuery(Post::class.java)
+        query.include(Post.KEY_USER)
+        query.findInBackground(object: FindCallback<Post> {
+            override fun done(posts: MutableList<Post>?, e: ParseException?) {
+                if (e !=null) {
+                    // Something went wrong
+                    Log.e(TAG, "Error fetching posts")
+                }
+                else {
+                    if (posts != null) {
+                        // If we got something
+                        for (post in posts){
+                            Log.i(TAG, "Post: "+post.getDescription() + " , username: " + post.getUser()?.username )
+                        }
+                    }
+                }
+            }
+        })
+
+    }
+    companion object {
+        const val TAG = "MainActivity"
+    }
+}
